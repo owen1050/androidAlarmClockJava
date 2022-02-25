9de447e72f56f0494a5fd70623d587d028f21013
Owen Busler <Owbusler@amazon.com>
2022-02-25 17:14:45 -0500

    notification/alarm works

--- app/src/main/java/com/example/alarmclock/Alarm.java
+++ app/src/main/java/com/example/alarmclock/Alarm.java
@@ -1,4 +1,10 @@
+
 package com.example.alarmclock;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -7,18 +13,52 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.media.MediaPlayer;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.alarmclock.MainActivity;
+import com.example.alarmclock.R;
 
 public class Alarm extends BroadcastReceiver {
-    MediaPlayer mp;
+
+    public MediaPlayer mp;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        if ((Intent.ACTION_BOOT_COMPLETED).equals(intent.getAction())) {
+            // reset all alarms
+        } else {
+            System.out.println("ALARM RAN");
 
-      //cant get app to open
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+            }
 
-        mp=MediaPlayer.create(context, R.raw.alarm);
-        mp.start();
+            Intent nIntent = new Intent(context, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, nIntent, 0);
 
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "1050")
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setContentTitle("title")
+                    .setContentText("content of notifiction")
+                    .setContentIntent(pendingIntent)
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+            notificationManager.notify(12345, builder.build());
+
+            //pull this out into service https://stackoverflow.com/questions/19581558/i-want-to-create-a-global-mediaplayer-that-will-keep-playing-no-matter-which-lay
+            mp=MediaPlayer.create(context, R.raw.alarm);
+            mp.start();
+
+        }
 
 
     }
+
 }
--- app/src/main/java/com/example/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.alarmclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,6 +13,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.KeyEvent;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setEditTextListeners();
         setTimes();
+        createNotificationChannel();
         setAlarm();
         System.out.println("##################on create finoshed");
 
@@ -57,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveTimes(){
+
         SharedPreferences sharedPreferences = getSharedPreferences("AlarmTimes",MODE_PRIVATE);
         SharedPreferences.Editor myEdit = sharedPreferences.edit();
         for(int i = 0; i < numAlarms; i++){
@@ -72,11 +78,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setAlarm(){
-        Intent intent = new Intent(this.getApplicationContext(), Alarm.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                this.getApplicationContext(), 234324243, intent, PendingIntent.FLAG_IMMUTABLE);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ (1 * 1000), pendingIntent);
+        System.out.println("Started alarm");
+
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, Alarm.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 1050, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent);
+
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "owenAlarmApp";
+            String description = "alarm app made by owen";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1050", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
